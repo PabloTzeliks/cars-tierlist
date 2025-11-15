d5842e7a48200621baa894c791aebf3d2f27882d
PabloTzeliks <arq.pabloo@gmail.com>
2025-11-14 21:55:34 -0300

    feat: adiciona fotos e lógica de script

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const draggableItems = document.querySelectorAll("#image-pool");
+  const dropzones = document.querySelectorAll(".drop-zone, .image-pool");
 
-    const draggableItems = document.querySelectorAll('#image-pool');
-    const dropZone = document.querySelectorAll('.tier-label, .image-pool');
+  draggableItems.forEach((item) => {
+    item.addEventListener("dragstart", (event) => {
+      event.dataTransfer.setData("text/plain", event.target.id);
 
-    draggableItems.forEach(item => {
-
-        item.addEventListener('dragstart', (event) => {
-
-            event.dataTransfer.setData('text/plain', event.target.id);
-
-            setTimeout(() => {
-
-                item.classList.add('dragging');
-            });
-        });
+      setTimeout(() => {
+        item.classList.add("dragging");
+      });
     });
 
+    item.addEventListener("dragstart", (event) => {
+      item.classList.remove("dragging");
+    });
+  });
+
+  dropzones.forEach((label) => {
+    
+    label.addEventListener("dragenter", (event) => {
+      
+      event.preventDefault();
+
+      label.classList.add("drag-over");
+    });
+
+    label.addEventListener("dragover", (event) => {
+      
+      event.preventDefault();
+    });
+
+    label.addEventListener("dragleave", () => {
+      
+      label.classList.remove("drag-over");
+    });
+
+    label.addEventListener('drop', (event) => {
+        
+        event.preventDefault();
+        
+        label.classList.remove('drag-over');
+        
+        const id = event.dataTransfer.getData('text/plain');
+        
+        const draggableElement = document.getElementById(id);
+        
+        if (draggableElement) {
+            label.appendChild(draggableElement);
+        }
+    });
+  });
 });
